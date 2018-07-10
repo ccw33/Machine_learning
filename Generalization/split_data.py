8d8b2c5fbcf3e4bc54859d6ae63d64fa61500115
ccw <952424500@qq.com>
2018-07-10 18:11:43 +0800

    验证 编程练习

--- Generalization/split_data.py
+++ Generalization/split_data.py
@@ -25,7 +25,7 @@ california_housing_dataframe = pd.read_csv("file/california_housing_train.csv", 
 # california_housing_dataframe = california_housing_dataframe.reindex(
 #     np.random.permutation(california_housing_dataframe.index))
 
-
+#我们首先加载并准备数据。这一次，我们将使用多个特征，因此我们会将逻辑模块化，以对特征进行预处理：
 def preprocess_features(california_housing_dataframe):
   """Prepares input features from California housing data set.
 
@@ -68,45 +68,249 @@ def preprocess_targets(california_housing_dataframe):
   return output_targets
 
 
-# 对于训练集，我们从共 17000 个样本中选择前 12000 个样本。
-training_examples = preprocess_features(california_housing_dataframe.head(12000))
-training_examples.describe()
-training_targets = preprocess_targets(california_housing_dataframe.head(12000))
-training_targets.describe()
+# 划分训练集和验证集
+def divide_ts_and_vs(california_housing_dataframe):
+    '''
+        划分训练集和验证集并返回，同时打印两个集在地图上的数据分布（看是否合理）
 
-#  对于**验证集**，我们从共 17000 个样本中选择后 5000 个样本。
-validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
-validation_examples.describe()
-validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
-validation_targets.describe()
+    :param california_housing_dataframe:
+    :return: training_examples,training_targets,validation_examples,validation_targets
+    '''
+    # 对于训练集，我们从共 17000 个样本中选择前 12000 个样本。
+    training_examples = preprocess_features(california_housing_dataframe.head(12000))
+    print(training_examples.describe())
+    training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+    print(training_targets.describe())
+
+    #  对于**验证集**，我们从共 17000 个样本中选择后 5000 个样本。
+    validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+    print(validation_examples.describe())
+    validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+    print(validation_targets.describe())
 
 
-# 绘制纬度/经度与房屋价值中位数的曲线图
+    # 绘制纬度/经度与房屋价值中位数的曲线图
 
-plt.figure(figsize=(13, 8))
+    plt.figure(figsize=(13, 8))
 
-ax = plt.subplot(1, 2, 1)
-ax.set_title("Validation Data")
+    ax = plt.subplot(1, 2, 1)
+    ax.set_title("Validation Data")
 
-ax.set_autoscaley_on(False)
-ax.set_ylim([32, 43])
-ax.set_autoscalex_on(False)
-ax.set_xlim([-126, -112])
-plt.scatter(validation_examples["longitude"],
-            validation_examples["latitude"],
-            cmap="coolwarm",
-            c=validation_targets["median_house_value"] / validation_targets["median_house_value"].max())
+    ax.set_autoscaley_on(False)
+    ax.set_ylim([32, 43])
+    ax.set_autoscalex_on(False)
+    ax.set_xlim([-126, -112])
+    plt.scatter(validation_examples["longitude"],
+                validation_examples["latitude"],
+                cmap="coolwarm",
+                c=validation_targets["median_house_value"] / validation_targets["median_house_value"].max())
 
-ax = plt.subplot(1,2,2)
-ax.set_title("Training Data")
+    ax = plt.subplot(1,2,2)
+    ax.set_title("Training Data")
 
-ax.set_autoscaley_on(False)
-ax.set_ylim([32, 43])
-ax.set_autoscalex_on(False)
-ax.set_xlim([-126, -112])
-plt.scatter(training_examples["longitude"],
-            training_examples["latitude"],
-            cmap="coolwarm",
-            c=training_targets["median_house_value"] / training_targets["median_house_value"].max())
-_ = plt.plot()
+    ax.set_autoscaley_on(False)
+    ax.set_ylim([32, 43])
+    ax.set_autoscalex_on(False)
+    ax.set_xlim([-126, -112])
+    plt.scatter(training_examples["longitude"],
+                training_examples["latitude"],
+                cmap="coolwarm",
+                c=training_targets["median_house_value"] / training_targets["median_house_value"].max())
+    _ = plt.plot()
+    plt.show()
+
+    return training_examples,training_targets,validation_examples,validation_targets
+
+divide_ts_and_vs(california_housing_dataframe)
+
+# 通过查看图像可以发现，数据在地图上的训练数据和验证数据分布不同，这是因为没有打乱数据而按顺序获取的原因所导致的（所以要打乱数据）
+# 同时查看统计表，发现有几项数据的最大最小值与25%75%的值相差太大（），很可能是坏数据 （所以要去掉离群值）
+# 机器学习中的调试通常是数据调试而不是代码调试。
+
+#1、去掉离群值
+# 绘制 rooms_per_person 的直方图
+plt.figure(figsize=(15, 6))
+plt.subplot(1, 1, 1)
+california_housing_dataframe["rooms_per_person"] =california_housing_dataframe["total_rooms"] / california_housing_dataframe["population"]
+_ = california_housing_dataframe["rooms_per_person"].hist()
 plt.show()
+# 创建的直方图显示，大多数值都小于 5。我们将 rooms_per_person 的值截取小于等于5的数，然后绘制直方图以再次检查结果。
+california_housing_dataframe["rooms_per_person"] = (
+    california_housing_dataframe["rooms_per_person"]).apply(lambda x: min(x, 5))
+_ = california_housing_dataframe["rooms_per_person"].hist()
+plt.show()
+
+#2、打乱数据
+california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe.index))
+training_examples,training_targets,validation_examples,validation_targets = divide_ts_and_vs(california_housing_dataframe)
+
+# 定义一下以前将数据加载到 TensorFlow 模型中时所使用的同一输入函数
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
+
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+
+    # Convert pandas data into a dict of np arrays.
+    features = {key: np.array(value) for key, value in dict(features).items()}
+
+    # Construct a dataset, and configure batching/repeating
+    ds = Dataset.from_tensor_slices((features, targets))  # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # Shuffle the data, if specified
+    if shuffle:
+        ds = ds.shuffle(10000)
+
+    # Return the next batch of data
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+# 由于我们现在使用的是多个输入特征，因此需要把用于将特征列配置为独立函数的代码模块化
+def construct_feature_columns(input_features):
+  """Construct the TensorFlow Feature Columns.
+
+  Args:
+    input_features: The names of the numerical input features to use.
+  Returns:
+    A set of feature columns
+  """
+  return set([tf.feature_column.numeric_column(my_feature)
+              for my_feature in input_features])
+
+# train_model() 代码，以设置输入函数和计算预测。
+def train_model(
+        learning_rate,
+        steps,
+        batch_size,
+        training_examples,
+        training_targets,
+        validation_examples,
+        validation_targets):
+    """Trains a linear regression model of one feature.
+
+    In addition to training, this function also prints training progress information,
+    as well as a plot of the training and validation loss over time.
+
+    Args:
+      learning_rate: A `float`, the learning rate.
+      steps: A non-zero `int`, the total number of training steps. A training step
+        consists of a forward and backward pass using a single batch.
+      batch_size: A non-zero `int`, the batch size.
+      training_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for training.
+      training_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for training.
+      validation_examples: A `DataFrame` containing one or more columns from
+        `california_housing_dataframe` to use as input features for validation.
+      validation_targets: A `DataFrame` containing exactly one column from
+        `california_housing_dataframe` to use as target for validation.
+
+    Returns:
+      A `LinearRegressor` object trained on the training data.
+    """
+
+    periods = 10
+    steps_per_period = steps / periods
+
+    # Create a linear regressor object.
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+    linear_regressor = tf.estimator.LinearRegressor(
+        feature_columns=construct_feature_columns(training_examples),
+        optimizer=my_optimizer
+    )
+
+    # Create input functions.
+    training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value"],
+        batch_size=batch_size)
+    predict_training_input_fn = lambda: my_input_fn(
+        training_examples,
+        training_targets["median_house_value"],
+        num_epochs=1,
+        shuffle=False)
+    predict_validation_input_fn = lambda: my_input_fn(
+        validation_examples, validation_targets["median_house_value"],
+        num_epochs=1,
+        shuffle=False)
+
+    # Train the model, but do so inside a loop so that we can periodically assess
+    # loss metrics.
+    print("Training model...")
+    print("RMSE (on training data):")
+    training_rmse = []
+    validation_rmse = []
+    for period in range(0, periods):
+        # Train the model, starting from the prior state.
+        linear_regressor.train(
+            input_fn=training_input_fn,
+            steps=steps_per_period,
+        )
+        # Take a break and compute predictions.
+        training_predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
+        training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+
+        validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
+        validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+
+        # Compute training and validation loss.
+        training_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(training_predictions, training_targets))
+        validation_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(validation_predictions, validation_targets))
+        # Occasionally print the current loss.
+        print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+        # Add the loss metrics from this period to our list.
+        training_rmse.append(training_root_mean_squared_error)
+        validation_rmse.append(validation_root_mean_squared_error)
+    print("Model training finished.")
+
+    # Output a graph of loss metrics over periods.
+    plt.ylabel("RMSE")
+    plt.xlabel("Periods")
+    plt.title("Root Mean Squared Error vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_rmse, label="training")
+    plt.plot(validation_rmse, label="validation")
+    plt.legend()
+    plt.show()
+
+    return linear_regressor
+
+linear_regressor = train_model(
+    learning_rate=0.00003*0.7,
+    steps=500,
+    batch_size=5,
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+
+# 基于测试数据进行评估
+california_housing_test_data = pd.read_csv("file/california_housing_test.csv", sep=",")
+test_examples = preprocess_features(california_housing_test_data)
+test_targets = preprocess_targets(california_housing_test_data)
+
+predict_test_input_fn = lambda: my_input_fn(
+      test_examples,
+      test_targets["median_house_value"],
+      num_epochs=1,
+      shuffle=False)
+
+test_predictions = linear_regressor.predict(input_fn=predict_test_input_fn)
+test_predictions = np.array([item['predictions'][0] for item in test_predictions])
+
+root_mean_squared_error = math.sqrt(
+    metrics.mean_squared_error(test_predictions, test_targets))
+
+print("Final RMSE (on test data): %0.2f" % root_mean_squared_error)
